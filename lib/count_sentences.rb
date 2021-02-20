96ea36a77319376e17d3e5b464dfd4f5ed2f2e95
Omar Branez <omar.branez@gmail.com>
2021-02-20 10:42:36 -0500

    commit tree 4322ea0ad4f3266afe3af3f98d345e6cb63cbc27

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return TRUE
+    else 
+      return FALSE
+    end    
   end
 
   def question?
-
+    if self.end_with?("?")
+      return TRUE
+    else
+      return FALSE
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return TRUE
+    else
+      return FALSE
+    end
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).collect.count
   end
 end
